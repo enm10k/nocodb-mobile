6169070c96079880e59c6c23cb13f41eb90b855f
enm10k <enm10k@gmail.com>
2024-06-29 22:20:28 +0900

    Remove unused code

--- lib/common/extensions.dart
+++ lib/common/extensions.dart
@@ -1,15 +1,3 @@
-import 'package:flutter/material.dart';
-
-extension ContextExtension on BuildContext {
-  ThemeData get theme => Theme.of(this);
-
-  Size get size => MediaQuery.of(this).size;
-
-  double get height => MediaQuery.of(this).size.height;
-
-  double get width => MediaQuery.of(this).size.width;
-}
-
 extension StringExtension on String {
   String capitalize() => '${this[0].toUpperCase()}${substring(1)}';
 }
@@ -21,12 +9,4 @@ extension JsonListExtension on Iterable<Map<String, dynamic>> {
           return a;
         },
       );
-}
-
-extension ListExtension<E> on List<E> {
-  List<E> u() => List<E>.unmodifiable(this);
-}
-
-extension MapExtension<K, V> on Map<K, V> {
-  Map<K, V> u() => Map<K, V>.unmodifiable(this);
 }
