6ec3ea6bfd6c0ca483b74218c18cf7259e9f1bed
enm10k <enm10k@gmail.com>
2024-07-01 17:43:13 +0900

    Fix isCloud

--- lib/nocodb_sdk/utils.dart
+++ lib/nocodb_sdk/utils.dart
@@ -1,1 +1,6 @@
-bool isCloud(String host) => host.contains('app.nocodb.com');
+final _cloudHosts = [
+  'app.nocodb.com',
+  ...(const String.fromEnvironment('NC_CLOUD_HOSTS').split(',')),
+];
+
+bool isCloud(String host) => _cloudHosts.any((h) => host.contains(h));
