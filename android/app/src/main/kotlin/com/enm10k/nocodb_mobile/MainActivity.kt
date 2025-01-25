7f5e5251ade468142b2d7a1e99548436d5771ae4
enm10k <enm10k@gmail.com>
2025-01-26 00:23:41 +0900

    Bump Flutter SDK to 3.27.3

--- android/app/src/main/kotlin/com/enm10k/nocodb_mobile/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.enm10k.nocodb_mobile
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,21 +1,13 @@
-import UIKit
 import Flutter
-import flutter_downloader
+import UIKit
 
-@UIApplicationMain
+@main
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
-    FlutterDownloaderPlugin.setPluginRegistrantCallback(registerPlugins)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
-}
-
-private func registerPlugins(registry: FlutterPluginRegistry) {
-    if (!registry.hasPlugin("FlutterDownloaderPlugin")) {
-       FlutterDownloaderPlugin.register(with: registry.registrar(forPlugin: "FlutterDownloaderPlugin")!)
-    }
 }
